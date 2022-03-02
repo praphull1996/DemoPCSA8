4c1baff64d9749a77820a1682ad7bc3456cee83f
praphull1996 <100759082+praphull1996@users.noreply.github.com>
2022-03-02 14:49:56 +0530

    Update demo.java

--- src/selenium/demo.java
+++ src/selenium/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("hi heloo    hihhf");
 		/// Hiii github 
+		// src - selenium -demo - click on pen - click on commit changes- open eclipse -right click on project-click on team-click on 1st pull-close
 	}
 
 }
